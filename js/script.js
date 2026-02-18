20581f05c26c4fd93733331f1e18bf42c7cb714a
majk.mone <mone.majk@gmail.com>
2026-02-18 16:06:43 +0100

    Edit Template

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 function getPrice() {
-	const age = parseInt(document.getElementById('age').value)
+	const nameSurname = document.getElementById('nameSurname').value
+	const age = document.getElementById('ageRange').value
 	const km = parseFloat(document.getElementById('km').value)
 
 	let price = km * 0.21
 
-	if (age < 18) {
+	if (age === 'Minorenne') {
 		price *= 0.8
-	} else if (age >= 65) {
+	} else if (age === 'Over 65') {
 		price *= 0.6
 	}
 
-	document.getElementById('showAge').textContent = age || '-'
-	document.getElementById('showKm').textContent = `${km || '-'} km`
+	document.getElementById('showNameSurname').textContent = nameSurname || '-'
+	document.getElementById('seniority').textContent = `${age || '-'}`
 	document.getElementById('showPrice').textContent = `${price.toFixed(2)} €`
+}
+
+function reset() {
+	document.getElementById('ticketForm').reset()
 }
