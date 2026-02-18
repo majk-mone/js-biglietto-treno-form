90485b07365344801c298f2ff9080dad593c7baf
majk.mone <mone.majk@gmail.com>
2026-02-18 16:12:11 +0100

    add randomInt()

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ function getPrice() {
 	const nameSurname = document.getElementById('nameSurname').value
 	const age = document.getElementById('ageRange').value
 	const km = parseFloat(document.getElementById('km').value)
+	let carrozza = randomInt(1, 12)
+	let codiceCP = randomInt(100, 9999)
 
 	let price = km * 0.21
 
@@ -14,8 +16,14 @@ function getPrice() {
 	document.getElementById('showNameSurname').textContent = nameSurname || '-'
 	document.getElementById('seniority').textContent = `${age || '-'}`
 	document.getElementById('showPrice').textContent = `${price.toFixed(2)} €`
+	document.getElementById('carrozza').textContent = `${carrozza || '-'}`
+	document.getElementById('codiceCP').textContent = `${codiceCP || '-'}`
 }
 
 function reset() {
 	document.getElementById('ticketForm').reset()
+}
+
+function randomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min
 }
